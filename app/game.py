a3c39aa7e79be77a466f6d7c3b80ea33fc9499a5
Sander Huijsen <sander.huijsen@gmail.com>
2020-08-01 13:58:57 +0800

    Allow the user to venture a guess.

--- app/game.py
+++ app/game.py
@@ -9,6 +9,9 @@ class Game:
         # Create a random secret number between 1 and 100 (inclusive)
         secret_number = random.randint(1, 100)
 
+        guess = input("What's your guess? ")
+        guess = int(guess)
+
 
 if __name__ == '__main__':
     game = Game()
