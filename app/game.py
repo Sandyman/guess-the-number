f3e9769851b4eeb577c65e9a7ce1bcaf66cfdf66
Sander Huijsen <sander.huijsen@gmail.com>
2020-08-01 14:02:54 +0800

    Run the game as a loop so that the player may try multiple times for the same secret.

--- app/game.py
+++ app/game.py
@@ -9,15 +9,17 @@ class Game:
         # Create a random secret number between 1 and 100 (inclusive)
         secret_number = random.randint(1, 100)
 
-        guess = input("What's your guess? ")
-        guess = int(guess)
+        while True:
+            guess = input("What's your guess? ")
+            guess = int(guess)
 
-        if guess == secret_number:
-            print("You win!")
-        elif guess < secret_number:
-            print("Oops. Your guess is too low! You lose!")
-        else:
-            print("Oops. Your guess is too high! You lose!")
+            if guess == secret_number:
+                print("You win!")
+                break
+            elif guess < secret_number:
+                print("Oops. Your guess is too low! You lose!")
+            else:
+                print("Oops. Your guess is too high! You lose!")
 
 
 if __name__ == '__main__':
