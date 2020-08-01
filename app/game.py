857f6e40dd9af0f3aa929078ce48067686b694bf
Sander Huijsen <sander.huijsen@gmail.com>
2020-08-01 13:57:52 +0800

    Import random. Create play method. Create secret number as a random number. Run file as script.

--- app/game.py
+++ app/game.py
@@ -1,4 +1,15 @@
+import random
+
+
 class Game:
     def __init__(self):
         self.score = 0
 
+    def play(self):
+        # Create a random secret number between 1 and 100 (inclusive)
+        secret_number = random.randint(1, 100)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.play()
